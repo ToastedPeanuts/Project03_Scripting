0c3f1f765d1b697699484b0271664cbda6d948ee
SBurke <samueljohnburke@yahoo.com>
2024-04-09 17:41:00 -0500

    SetActive Gameobject based on Trigger

--- Assets/TriggerVolume.cs
+++ Assets/TriggerVolume.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[DisallowMultipleComponent()]
+[RequireComponent(typeof(BoxCollider))]
 public class TriggerVolume : MonoBehaviour
 {
-    public UnityEvent onEnterTrigger;
+    public UnityEvent OnEnterTrigger;
+
+    private Collider _collider;
 
     private void Awake()
     {
-        
+        _collider = GetComponent<Collider>();
+        _collider.isTrigger = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //validate object; ie can certain things work with the trigger?
-        //Will call whatever is set in the list to trigger
         Debug.Log("Entered");
-        onEnterTrigger.Invoke();
-
-
+        OnEnterTrigger.Invoke();
     }
 }
