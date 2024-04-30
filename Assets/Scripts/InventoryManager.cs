83d531058c2acfeb973a6549c92e3b93ef912516
SBurke <samueljohnburke@yahoo.com>
2024-04-30 17:25:35 -0500

    Created Inventory slots/Connected slots to inventory Manager

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,6 +6,7 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject InventoryMenu;
     private bool menuActivated;
+    public ItemSlot[] itemSlot;
 
     private void Update()
     {
@@ -25,6 +26,14 @@ public class InventoryManager : MonoBehaviour
 
     public void AddItem(string itemName,int quantity, Sprite itemSprite)
     {
-        Debug.Log("itemName=" + itemName + "quantity=" + quantity + "itemSprite=" + itemSprite);
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if(itemSlot[i].isFull == false)
+            {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite);
+                return;
+            }
+
+        }
     }
 }
