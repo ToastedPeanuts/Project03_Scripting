345c0fbc1fad17fa7d12d8c01537bfef38296960
SBurke <samueljohnburke@yahoo.com>
2024-04-30 18:15:46 -0500

    Selecting Items in inventory menu

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //Item Data
     public string itemName;
@@ -19,6 +20,9 @@ public class ItemSlot : MonoBehaviour
     [SerializeField]
     private Image itemImage;
 
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
     public void AddItem(string itemName, int quantity, Sprite itemSprite) 
     {
         this.itemName = itemName;
@@ -31,4 +35,26 @@ public class ItemSlot : MonoBehaviour
         itemImage.sprite = itemSprite;
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left) 
+        {
+            OnLeftClick();
+        }
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+
+    public void OnRightClick()
+    {
+
+    }
 }
